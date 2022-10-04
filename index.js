bd37cf8f9dab3956cfdc03f5a53b021cc68ed720
varma6228@gmail.com <varma6228@gmail.com>
2022-10-04 14:35:36 -0400

    New Changes
    index.js
    package-lock.json
    package.json
    .gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const port = 8081;
+server.get('/',(req,res)=>{
+    res.send('<h1>HELLO WORLD...HOME!!!');
+    res.end();
+});
+
+server.get('/products',(req,res)=>{
+    res.send('<h1>PRODUCTS PAGE</h1>');
+    res.end();
+});
+
+server.get('/Profile',(req,res)=>{
+    res.send('<h1>PROFILE PAGE</h1>');
+});
+
+server.listen(port,()=>{
+    console.log('Server running on'+port);
+});
